58acaeadfcbfb6ecd0fd7803772178c0e1c7355b
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2025-01-10 10:16:16 +0100

    Card swiper redondemaos los bordes

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -12,16 +12,23 @@ class CardSwiper extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: size.height * 0.5,
-      color: Colors.red,
+      padding: const EdgeInsets.all(30),
       child: Swiper(
         itemCount: 10,
         layout: SwiperLayout.STACK,
         itemWidth: size.width*0.6,
         itemHeight: size.height*0.9,
         itemBuilder: (context, index) {
-          return FadeInImage(
-            placeholder: AssetImage('assets/no-image.jpg'), 
-            image: NetworkImage('https://static.posters.cz/image/750/posters/squid-game-crowd-i120012.jpg')
+          return GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://static.posters.cz/image/750/posters/squid-game-crowd-i120012.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
           );
         },
       )
