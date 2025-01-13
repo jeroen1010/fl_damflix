7de05596c4126ded0577cec68c9e24992bee9ad7
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2025-01-13 11:05:52 +0100

    Slider de las peliculas, overflow, texto con nombre de la peli y la imagen

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -22,18 +22,45 @@ class MovieSlider extends StatelessWidget {
             scrollDirection: Axis.horizontal,
             itemCount: 10,
             itemBuilder: (context, index) {
-              return Container(
-                width: 150,
-                height: 200,
-                color: Colors.blue,
-                margin: const EdgeInsets.all(10)
-              );
+              return _MoviePoster();
             }
           ),
         )
 
         ],
       ),
+    );
+  }
+}
+
+class _MoviePoster extends StatelessWidget {
+  const _MoviePoster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 190,
+      color: Colors.blue,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          //Cartel
+          FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'), 
+            image: NetworkImage('https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg'),
+            width: 130,
+            height: 190,
+            fit: BoxFit.cover,
+          ),
+
+          //Titulo
+          Text('Interstellar',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          )
+        ],
+        ),
     );
   }
 }
