5b1408379e497636e0cd40e10da77c74d2162eef
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2025-01-13 12:39:54 +0100

    Detail screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -7,10 +7,39 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
-          )
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      floating: true,
+      pinned: true,
+      snap: false,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          color: Colors.black54,
+          alignment: Alignment.bottomCenter,
+          child: Text('pelicula.titulo', style: TextStyle(color: Colors.white),)
+          ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://2.bp.blogspot.com/-Ezjj3Ih9eog/VWdJXjEXGPI/AAAAAAAACnA/Vk-fayxGzto/s1600/interstellar-wallpaper.jpg'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
